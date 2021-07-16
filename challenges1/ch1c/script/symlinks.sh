218b08b4d3f1776f83c73e991bc7f98bc43d6b2e
Kevin Korac <kevin.korac@carvolution.ch>
2021-07-16 15:02:42 +0200

    ch1c cleaned up

--- challenges1/ch1c/script/symlinks.sh
+++ challenges1/ch1c/script/symlinks.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-mkdir src/common
-mkdir lib
-#ln -s ../../../ch1a/src/common/*.java ../src/common/*.java
-
-ln -s ../../../ch1a/src/common/Customer.java src/common/Customer.java
-ln -s ../../../ch1a/src/common/CustomerService.java src/common/CustomerService.java
-ln -s ../../../ch1a/src/common/CustomerRepository.java src/common/CustomerRepository.java
-ln -s ../../../ch1a/src/common/AbstractController.java src/common/AbstractController.java
-ln -s ../../../ch1a/src/common/DBapi.java src/common/DBapi.java
-
-
-ln -s ../../ch1a/lib/mysql-connector-java-8.0.23.jar lib/mysql-connector-java-8.0.23.jar
-ln -s ../../ch1b/lib/hsqldb.jar lib/hsqldb.jar
+ln -s ../../ch1a/src/common/ src/common
