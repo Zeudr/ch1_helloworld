b05b740c73ee6ae432c8804d3f91dd2af71ec268
Kevin Korac <kevin.korac@carvolution.ch>
2021-07-16 16:02:28 +0200

    jar not working

--- challenges1/ch1c/script/symlinks.sh
+++ challenges1/ch1c/script/symlinks.sh
@@ -4,3 +4,6 @@ ln -s ../../ch1a/src/common/ src/common
 
 ln -s ../../ch1a/src/MySqlApi.java src/MySqlApi.java
 ln -s ../../ch1b/src/HSqlApi.java src/HSqlApi.java
+
+
+#jar cf lib/common.jar lib/hsqldb.jar lib/mysql-connector-java-8.0.23.jar
